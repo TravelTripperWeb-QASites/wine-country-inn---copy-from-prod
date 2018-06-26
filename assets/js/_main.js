b6f1d7a84ff6dde0257b52f1fb48239002076939
Srinivas <rsrinivas13@gmail.com>
2018-06-26 16:09:02 +0530

    blog search and category landing page added

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -200,3 +200,14 @@ $(document).ready(function () {
     }
 
 });
+
+
+// Pinterest
+
+function pinterestShare(img, desc) {
+    window.open("//www.pinterest.com/pin/create/button/" +
+        "?url=" + window.location.href +
+        "&media=" + img +
+        "&description=" + desc, "pinIt", "toolbar=no, scrollbars=no, resizable=no, top=0, right=0");
+    return false;
+}
